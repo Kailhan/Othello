b0ffa83620e46b6b2658739971d78dfbe33c1098
MaaikeHHSTRA <maaikehooghiemstra@live.nl>
2018-09-11 12:21:17 +0200

    Methods GameEngine

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,3 +1,29 @@
 public class GameEngine {
 
+    public int countBlack(){
+       return 0;
+    }
+
+    public int countWhite(){
+        return 0;
+    }
+
+    //change color; who's turn; only coordinate as input
+    public void changeColor(int x, int y){
+
+    }
+
+    public boolean checkSquareAllowed(){
+        return false;
+    }
+
+    public int numberSquaresAllowed(){
+
+    }
+
+    public enum turn(){
+
+    }
+
+
 }
