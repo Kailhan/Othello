d10c49570750ebbb17aee9218e15248b35cbf1ce
Takowski <takowski@outlook.com>
2018-09-24 00:36:07 +0200

    proof of conceptish flipping tiles

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -4,7 +4,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -24,11 +23,19 @@ public class GameScreen extends Application {
     private int gap = 5;
     private int tileSize;
 
-    File discBlack = new File("src/Assets/disc_blackBgr.png");
-    File discWhite = new File("src/Assets/disc_whiteBgr.png");
-    File bgr = new File("src/Assets/Bgr.png");
-    File discBlackMenu = new File("src/Assets/disc_blackMenu.png");
-    File discWhiteMenu = new File("src/Assets/disc_whiteMenu.png");
+    private File discBlack = new File("src/Assets/disc_blackBgr.png");
+    private File discWhite = new File("src/Assets/disc_whiteBgr.png");
+    private File bgr = new File("src/Assets/Bgr.png");
+    private File discBlackMenu = new File("src/Assets/disc_blackMenu.png");
+    private File discWhiteMenu = new File("src/Assets/disc_whiteMenu.png");
+    private Image discBlackImg;
+    private Image discWhiteImg;
+    private Image bgrImg;
+    private Image discBlackMenuImg;
+    private Image discWhiteMenuImg;
+    private Button button;
+    private ImageView discBlackMenuView;
+    private ImageView discWhiteMenuView;
 
     public static void main(String[] args) {
         launch(args);
@@ -39,13 +46,19 @@ public class GameScreen extends Application {
         this.board = new Board();
         this.boardGrid = board.getBoardGrid();
         this.tileSize = windowSize/ boardGrid[0].length;
+        this.discBlackImg = new Image(discBlack.toURI().toString(), tileSize, tileSize, false,false);
+        this.discWhiteImg = new Image(discWhite.toURI().toString(), tileSize, tileSize, false,false);
+        this.bgrImg = new Image(bgr.toURI().toString(), tileSize, tileSize, false,false);
+        this.discBlackMenuImg = new Image(discBlackMenu.toURI().toString(), tileSize, tileSize, false,false);
+        this.discWhiteMenuImg = new Image(discWhiteMenu.toURI().toString(), tileSize, tileSize, false,false);
+        this.discBlackMenuView = new ImageView(discBlackMenuImg);
+        this.discWhiteMenuView = new ImageView(discWhiteMenuImg);
+        this.button = new Button("Start a game");
         this.window = primaryStage;
-        window.setTitle("Othello");
 
-        //grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setGridLinesVisible(false);
-        grid.setVgap(gap);
-        grid.setHgap(gap);
+        //grid.setVgap(gap);
+        //grid.setHgap(gap);
 
         redrawBoard();
 
@@ -53,44 +66,35 @@ public class GameScreen extends Application {
         bPane.setCenter(grid); //can directly create scene from grid if borderpane layout is not gonna be used
 
         Scene scene = new Scene(bPane, windowSize + tileSize*3 + gap*(boardGrid.length+2), windowSize + gap*(boardGrid.length-1), Color.rgb(128, 128, 128));
+        window.setTitle("Othello");
         window.setScene(scene);
         window.show();
     }
 
     public void redrawBoard (){
-        Image discBlackImg = new Image(discBlack.toURI().toString(), tileSize, tileSize, false,false);
-        Image discWhiteImg = new Image(discWhite.toURI().toString(), tileSize, tileSize, false,false);
-        Image bgrImg = new Image(bgr.toURI().toString(), tileSize, tileSize, false,false);
-        Image discBlackMenuImg = new Image(discBlackMenu.toURI().toString(), tileSize, tileSize, false,false);
-        Image discWhiteMenuImg = new Image(discWhiteMenu.toURI().toString(), tileSize, tileSize, false,false);
-        Button button = new Button("Start a game");
-
-        List<ImageView> toAdd = new ArrayList<>();
+        List<Button> toAdd = new ArrayList<>();
         for (int r = 0; r < boardGrid.length; r++) {
             for (int c = 0; c < boardGrid[r].length; c++) {
                 if(boardGrid[r][c] == 0) {
-                    toAdd.add(new ImageView(bgrImg));
+                    toAdd.add(new Button(null, new ImageView(bgrImg)));
                 }
                 if(boardGrid[r][c] == 1) {
-                    toAdd.add(new ImageView(discBlackImg));
+                    toAdd.add(new Button(null, new ImageView(discBlackImg)));
                 }
                 if(boardGrid[r][c] == 2) {
-                    toAdd.add(new ImageView(discWhiteImg));
+                    toAdd.add(new Button(null, new ImageView(discWhiteImg)));
                 }
-                toAdd.get(toAdd.size()-1).addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-                    if(1==1) {
-                         //= new ImageView(bgrImg);
-                    }
-                    event.consume();
+                toAdd.get(toAdd.size()-1).setId(r+","+c);
+                toAdd.get(toAdd.size()-1).setOnAction((event) -> {
+                    Button button = (Button)event.getSource();
+                    updateBoard(button.getId());
                 });
                 GridPane.setConstraints(toAdd.get(toAdd.size()-1), r, c);
             }
         }
 
-        toAdd.add(new ImageView(discBlackMenuImg));
-        GridPane.setConstraints(toAdd.get(toAdd.size()-1), boardGrid.length, 3);
-        toAdd.add(new ImageView(discWhiteMenuImg));
-        GridPane.setConstraints(toAdd.get(toAdd.size()-1), boardGrid.length + 2, 3);
+        GridPane.setConstraints(discBlackMenuView, boardGrid.length, 3);
+        GridPane.setConstraints(discWhiteMenuView, boardGrid.length + 2, 3);
         GridPane.setConstraints(button, boardGrid.length, 5);
 
         Label blackDiscs = new Label(Integer.toString(board.getNumberOfBlackSquares()));
@@ -99,6 +103,13 @@ public class GameScreen extends Application {
         GridPane.setConstraints(whiteDiscs, boardGrid.length + 2, 4);
 
         grid.getChildren().addAll(toAdd);
-        grid.getChildren().addAll(button, blackDiscs, whiteDiscs);
+        grid.getChildren().addAll(button, discBlackMenuView, discWhiteMenuView, blackDiscs, whiteDiscs);
+    }
+
+    public void updateBoard(String ID) {
+        int x = Integer.parseInt(ID.split("\\,")[0]);
+        int y = Integer.parseInt(ID.split("\\,")[1]);
+        boardGrid[x][y] = 1;
+        redrawBoard();
     }
 }
