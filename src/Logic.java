356faa3deb7c00ae56768466cec5539886e906f2
Rytjens <37538658+Rytjens@users.noreply.github.com>
2018-09-26 13:51:34 +0200

    generalising logic and equalize board syntax

--- src/Logic.java
+++ src/Logic.java
@@ -1,33 +1,13 @@
 public class Logic {
 
-    private Board grid;
-    private int[][] board;
-    private int turn = 1;
-
-    public Logic(Board grid) {
-        this.grid = grid;
-        this.board = grid.getBoardGrid();
-    }
-
-    public int countBlack() {
-        int nrBlack = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 1) nrBlack++;
-            }
-        }
-        return nrBlack;
-    }
-
-    public int countWhite(){
-        int nrWhite = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 1) nrWhite++;
-            }
-        }
-        return nrWhite;
-    }
+//    private Board grid;
+//    private int[][] board;
+//    private int turn = 1;
+//
+//    public Logic(Board grid) {
+//        this.grid = grid;
+//        this.board = grid.getBoardGrid();
+//    }
 
     //change color; who's turn; only coordinate as input
     /*
@@ -39,152 +19,188 @@ public class Logic {
     */
 
     //only returns if it possible to place a disk on the selected square (NOT: show the disks that would switch color)
-    public boolean checkSquareAllowed(int x, int y){
-        int sizeBoard = grid.getSize();
-        int playerNr = 1;
-        int edgeBoardNeg = -1;
+    public static boolean checkSquareAllowed(int x, int y, Board board, int playerNr) {
+        int[][] boardGrid = board.getBoardGrid();
+        int boardSize = board.getSize();
 
-        if(board[x][y] == 0) { //square is empty
-            //if (x == 0 && y == 0) {
-            if (turn % 2 != 0) {
-                playerNr = 1; //player 1 Black
-            } else {
-                playerNr = 2; //player 2 White
-            }
+        if (boardGrid[x][y] == 0) {
+            if (x != 0 && x != boardSize - 1 && y != 0 && y != boardSize - 1) {
 
-            if (x == 0)
+            }
+            else if (x == 0 && y != 0 && y != boardSize - 1) {
 
-                //check on same color in row/diagonal/colomn
-                for (int i = y - 1; i > edgeBoardNeg; i--) { //vertical - North
-                    if (y == 0) {
-                        break;
-                    }
-                    if (board[x][i] == 0) {
-                        break;
-                    }
-                    if (board[x][i] == playerNr) {
-                        return true;
-                    } //disk is same color as the player
-                }
-            for (int i = y - 1; i > edgeBoardNeg; i--) { //diagonal - NorthEast
-                for (int j = x + 1; j < sizeBoard; j++) {
-                    if (y == 0 || x == sizeBoard - 1) {
-                        break;
-                    }
-                    if (board[i][j] == 0) {
-                        break;
-                    }
-                    if (board[i][j] == playerNr) {
-                        return true;
-                    } //disk is same color as the player
-                }
             }
-            for (int i = x + 1; i < sizeBoard; i++) { //horizontal - East
-                if (x == sizeBoard - 1) {
-                    break;
-                }
-                if (board[i][y] == 0) {
-                    break;
-                }
-                if (board[i][y] == playerNr) {
-                    return true;
-                } //disk is same color as the player
+            else if (x == boardSize - 1 && y != 0 && y != boardSize - 1) {
+
             }
-            for (int i = y + 1; i < sizeBoard; i++) { //diagonal - EastSouth
-                for (int j = x + 1; j < sizeBoard; j++) {
-                    if (y == sizeBoard - 1 || x == sizeBoard - 1) {
-                        break;
-                    }
-                    if (board[i][j] == 0) {
-                        break;
-                    }
-                    if (board[i][j] == playerNr) {
-                        return true;
-                    } //disk is same color as the player
-                }
+            else if (x != 0 && x != boardSize - 1 && y == 0) {
+
             }
-            for (int i = y + 1; i < sizeBoard; i++) { //vertical - South
-                if (y == sizeBoard - 1) {
-                    break;
-                }
-                if (board[x][i] == 0) {
-                    break;
-                }
-                if (board[x][i] == playerNr) {
-                    return true;
-                } //disk is same color as the player
+            else if (x != 0 && x != boardSize - 1 && y == boardSize - 1) {
+
             }
-            for (int i = y + 1; i < sizeBoard; i++) { //diagonal - SouthWest
-                for (int j = x - 1; j > edgeBoardNeg; j--) {
-                    if (y == sizeBoard - 1 || x == 0) {
-                        break;
-                    }
-                    if (board[i][j] == 0) {
-                        break;
-                    }
-                    if (board[i][j] == playerNr) {
-                        return true;
-                    } //disk is same color as the player
-                }
+            else if (x == 0 && y == 0) {
+
             }
-            for (int i = x - 1; i > edgeBoardNeg; i--) { //horizontal - East
-                if (x == 0) {
-                    break;
-                }
-                if (board[i][y] == 0) {
-                    break;
-                }
-                if (board[i][y] == playerNr) {
-                    return true;
-                } //disk is same color as the player
+            else
+            if (x != 0 && x != boardSize - 1 && y != 0 && y != boardSize - 1) {
+
             }
-            for (int i = y - 1; i > edgeBoardNeg; i--) { //diagonal - WestNorth
-                for (int j = x - 1; j > edgeBoardNeg; j--) {
-                    if (x == 0 || y == 0) {
-                        break;
-                    }
-                    if (board[i][j] == 0) {
-                        break;
-                    }
-                    if (board[i][j] == playerNr) {
-                        return true;
-                    } //disk is same color as the player
-                }
-            }
-            return false;
         }
         return false;
     }
 
+    public static boolean checkDirectionAllowed(int x, int y, Board board, int playerNr, String direction)
+    {
+        switch(direction)
+        {
+            case "N" :
+            case "NE" :
+            case "E" :
+            case "SE" :
+            case "S" :
+            case "SW" :
+            case "W" :
+            case "NW" :
+
+            default:
+
+        }
+        return false;
+    }
+
+//        int sizeBoard = grid.getSize();
+//        int playerNr = 1;
+//        int edgeBoardNeg = -1;
+//
+//        if(board[x][y] == 0) { //square is empty
+//            //if (x == 0 && y == 0) {
+//            if (turn % 2 != 0) {
+//                playerNr = 1; //player 1 Black
+//            } else {
+//                playerNr = 2; //player 2 White
+//            }
+//
+//            if (x == 0)
+//
+//                //check on same color in row/diagonal/colomn
+//                for (int i = y - 1; i > edgeBoardNeg; i--) { //vertical - North
+//                    if (y == 0) {
+//                        break;
+//                    }
+//                    if (board[x][i] == 0) {
+//                        break;
+//                    }
+//                    if (board[x][i] == playerNr) {
+//                        return true;
+//                    } //disk is same color as the player
+//                }
+//            for (int i = y - 1; i > edgeBoardNeg; i--) { //diagonal - NorthEast
+//                for (int j = x + 1; j < sizeBoard; j++) {
+//                    if (y == 0 || x == sizeBoard - 1) {
+//                        break;
+//                    }
+//                    if (board[i][j] == 0) {
+//                        break;
+//                    }
+//                    if (board[i][j] == playerNr) {
+//                        return true;
+//                    } //disk is same color as the player
+//                }
+//            }
+//            for (int i = x + 1; i < sizeBoard; i++) { //horizontal - East
+//                if (x == sizeBoard - 1) {
+//                    break;
+//                }
+//                if (board[i][y] == 0) {
+//                    break;
+//                }
+//                if (board[i][y] == playerNr) {
+//                    return true;
+//                } //disk is same color as the player
+//            }
+//            for (int i = y + 1; i < sizeBoard; i++) { //diagonal - EastSouth
+//                for (int j = x + 1; j < sizeBoard; j++) {
+//                    if (y == sizeBoard - 1 || x == sizeBoard - 1) {
+//                        break;
+//                    }
+//                    if (board[i][j] == 0) {
+//                        break;
+//                    }
+//                    if (board[i][j] == playerNr) {
+//                        return true;
+//                    } //disk is same color as the player
+//                }
+//            }
+//            for (int i = y + 1; i < sizeBoard; i++) { //vertical - South
+//                if (y == sizeBoard - 1) {
+//                    break;
+//                }
+//                if (board[x][i] == 0) {
+//                    break;
+//                }
+//                if (board[x][i] == playerNr) {
+//                    return true;
+//                } //disk is same color as the player
+//            }
+//            for (int i = y + 1; i < sizeBoard; i++) { //diagonal - SouthWest
+//                for (int j = x - 1; j > edgeBoardNeg; j--) {
+//                    if (y == sizeBoard - 1 || x == 0) {
+//                        break;
+//                    }
+//                    if (board[i][j] == 0) {
+//                        break;
+//                    }
+//                    if (board[i][j] == playerNr) {
+//                        return true;
+//                    } //disk is same color as the player
+//                }
+//            }
+//            for (int i = x - 1; i > edgeBoardNeg; i--) { //horizontal - East
+//                if (x == 0) {
+//                    break;
+//                }
+//                if (board[i][y] == 0) {
+//                    break;
+//                }
+//                if (board[i][y] == playerNr) {
+//                    return true;
+//                } //disk is same color as the player
+//            }
+//            for (int i = y - 1; i > edgeBoardNeg; i--) { //diagonal - WestNorth
+//                for (int j = x - 1; j > edgeBoardNeg; j--) {
+//                    if (x == 0 || y == 0) {
+//                        break;
+//                    }
+//                    if (board[i][j] == 0) {
+//                        break;
+//                    }
+//                    if (board[i][j] == playerNr) {
+//                        return true;
+//                    } //disk is same color as the player
+//                }
+//            }
+//            return false;
+//        }
+//        return false;
+//    }
+
     //counts the amount of possible moves
-    public int numberSquaresAllowed() {
+    public int numberSquaresAllowed(Board board)
+    {
+        int boardSize = board.getSize();
         int nrOfAllowedSquares = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (checkSquareAllowed(i, j)) nrOfAllowedSquares++;
-            }
+        for (int i = 0; i < boardSize; i++)
+        {
+            for (int j = 0; j < boardSize; j++)
+                if (checkSquareAllowed(i, j, board, board.getCurrentPlayer()))
+                    nrOfAllowedSquares++;
             return nrOfAllowedSquares;
         }
         return 0;
     }
 
-    //disk flips or placed in an empty square
-    public int[][] applyMove(int xCoord, int yCoord) {
-        if(checkSquareAllowed(xCoord, yCoord)) {
-            if((board[xCoord][yCoord] == 0 || board[xCoord][yCoord] == 2) && turn == 1) board[xCoord][yCoord] = 1; //if the square is empty or white and it's black's turn, put a black disc in the square
-            if((board[xCoord][yCoord] == 0 || board[xCoord][yCoord] == 1) && turn == 2) board[xCoord][yCoord] = 2; //if the square is empty or black and it's white's turn, put a white disc in the square
-        } else {
-            System.out.println("Move not allowed");
-        }
-        changeTurn();
-        return board;
-    }
 
-    public void changeTurn(){
-        if (turn == 1) {
-            turn = 2;
-        } else {
-            turn = 1;
-        }
-    }
+
+
 }
