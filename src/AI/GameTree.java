86860890c46f1083f11e766defad03ba50e2a61b
yvar <yjfhulshof@gmail.com>
2018-11-04 17:43:40 +0100

    GameTree working

--- src/AI/GameTree.java
+++ src/AI/GameTree.java
@@ -9,81 +9,117 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/**
+ * The tree data structure which contains simulated boards, constructed layer by layer so that a specified depth can be used
+ */
+
 public class GameTree {
 
     private Logic logic;
     private Board board;
     private int[][] boardGrid;
-    //private int currentPossibleMoves[][];
-    private Node parent;
-    private int moveIndex;
     private int treeDepth;
-
 
     public GameTree(int treeDepth, Board board) {
         this.logic = new Logic();
         this.board = new Board();
         this.boardGrid = board.getBoardGrid();
-        this.parent = new Node(new int[2]);
         this.treeDepth = treeDepth;
-        //currentPossibleMoves = new int[boardGrid.length][2];
     }
 
     public GameTree(int treeDepth){
         this(treeDepth, new Board());
     }
 
-    public void createTreeLayer(Node root){
-        this.boardGrid = board.getBoardGrid();
-        //Node currentParent = new Node(new int[board.getSize()][board.getSize()]);
-        Node currentChild = new Node(new int[board.getSize()][board.getSize()]);
-        root.setData(boardGrid);
-        for(int depth = 0; depth < treeDepth; depth++) {
+    /**
+     Constructs one increment of a new layer of nodes by adding the children of a parent in the layer above to this new layer
+     The child nodes are created by applying moves to their parents' board and storing the resulting board
+     */
+
+    public ArrayList<Node<Board>> createLayerIncrement(Node<Board> root, ArrayList<Node<Board>> currentLayer){
+            Board rootBoard = root.getData();
             for (int i = 0; i < boardGrid.length; i++) {
                 for (int j = 0; j < boardGrid[0].length; j++) {
-                    if (logic.checkSquareAllowed(i, j, board)) {
-                        Board tmpBoard = new Board(board);
+                    if (logic.checkSquareAllowed(i, j, rootBoard)) {
+                        Board tmpBoard = new Board(rootBoard); //makes use of the deep-copy method in board so that the child and parents' board are distinct objects
                         tmpBoard.applyMove(i, j);
-                        int[][] boardGrid = tmpBoard.getBoardGrid();
-                        currentChild.setData(boardGrid);
-                        currentChild.setParent(root);
+                        tmpBoard.changePlayer();
+                        Node currentChild = new Node(tmpBoard);
+                        root.addChild(currentChild);
+                        currentLayer.add(currentChild);
                     }
                 }
             }
-            board.changePlayer();
-        }
+            return currentLayer;
     }
 
-    public Node createTree(){
-        Node root = new Node(new int[board.getSize()][board.getSize()]);
-        //List<Node<new int[board.getSize()][board.getSize()]>> children = new ArrayList<Node>(root.getChildren());
-        createTreeLayer(root);
-        for(int depth = 0; depth < treeDepth; depth++){
-            for(int childNr = 0; childNr < root.getChildren().size(); childNr++){
-                root = (Node) root.getChildren().get(childNr);
-                //createTreeLayer((Node) root.getChildren().get(childNr));
-                createTreeLayer(root);
+    /**
+     * Creates the tree layer by layer by calling createTreeLayer() for every node in the current layer
+     */
+
+    public Node<Board> createTree(){
+        Node<Board> root = new Node(board);
+        ArrayList<Node<Board>> currentLayer = new ArrayList<>();
+        ArrayList<Node<Board>> newLayer = new ArrayList<>();
+        currentLayer = createLayerIncrement(root, currentLayer); //creating the first layer below the root
+        for(int depth = 1; depth < treeDepth; depth++){
+            for(Node<Board> currentRoot: currentLayer){      //incrementally constructing the new layer
+                createLayerIncrement(currentRoot, newLayer); //by adding the children of all parents in this layer to the new layer
             }
-        }
-        while(root.getParent()!= null){
-            root = root.getParent();
+            currentLayer = new ArrayList<>(newLayer);
+            newLayer.clear(); //clearing ArrayList so that it can be used for the next layer
         }
         return root;
     }
 
-    public static void displayGrid(int[][] boardGrid){
+    public void displayGrid(Board board){
+        boardGrid = board.getBoardGrid();
+        String representation = "";
         for(int i = 0; i < boardGrid.length; i++){
             for(int j = 0; j < boardGrid[0].length; j++){
-                System.out.printf("%2d", boardGrid[i][j] + " ");
+                //System.out.printf("%2d", boardGrid[i][j] + " ");
+                if(boardGrid[i][j] == 0) representation = "0";
+                if(boardGrid[i][j] == 1) representation = "1";
+                if(boardGrid[i][j] == -1) representation = "2";
+                System.out.print(representation + " ");
             }
             System.out.println();
         }
     }
 
-//    public void displayTree(Node root) {
-//        Queue<Node> nodes = new LinkedList<Node>();
-//        nodes.add(root.getChildren());
-//        nodes.add(nodes.remove().getChildren());
-//    }
+    public void displayChildNodes(Node<Board> root){
+        List<Node<Board>> currentLayer;
+        currentLayer = root.getChildren();
+        ArrayList<Node<Board>> newLayer = new ArrayList<>();
+        //System.out.println("Layer: " + findLayerIndex(root));
+        for(Node<Board> currentChild: currentLayer){
+            System.out.println();
+            displayGrid(currentChild.getData());
+            newLayer.add(currentChild);
+        }
+        for(Node<Board> newChild: newLayer){
+            displayChildNodes(newChild);
+        }
+    }
+
+    /*
+    public int findLayerIndex(Node<Board> node) {
+        int[][] boardGrid = node.getData().getBoardGrid();
+        int discCounter = 0;
+        for (int i = 0; i < boardGrid.length; i++) {
+            for (int j = 0; j < boardGrid[0].length; j++) {
+                if (boardGrid[i][j] == -1 || boardGrid[i][j] == 1) discCounter++;
+            }
+        }
+            return discCounter - 3;
+    }
+
+
+   public void displayTree(Node root) {
+       Queue<Node> nodes = new LinkedList<Node>();
+       nodes.add(root.getChildren());
+       nodes.add(nodes.remove().getChildren());
+    }
+    */
 
 }
--- src/AI/GameTreeTest.java
+++ src/AI/GameTreeTest.java
@@ -6,15 +6,8 @@ public class GameTreeTest {
 
     public static void main(String[] args){
 
-        Board board = new Board(8);
-        GameTree T = new GameTree(1);
-        Node root = new Node(new int[board.getSize()][board.getSize()]);
-        root = T.createTree();
-        int[][] boardGrid = new int[8][8];
-        boardGrid = (int[][]) root.getData();
-        //displayGrid(boardGrid);
-        //T.displayTree(root);
+        GameTree T = new GameTree(3);
+        Node<Board> root = T.createTree();
+        T.displayChildNodes(root);
     }
-
-
 }
