813cd15370cc6dd0afffc5b7c4cf9c4b7de9a781
MartijnHilders <martijnhilders@live.nl>
2018-11-16 17:37:39 +0100

    ...

--- src/AI/MiniMaxAlph.java
+++ src/AI/MiniMaxAlph.java
@@ -25,70 +25,92 @@ public class MiniMaxAlph {
     }
 
 
-    public Node<Board> search(Node<Board> currentNode,int player/* int depth, double myBest, double theirBest*/){
+    public int search(Node<Board> currentNode,int player) {
         currentPlayer = player;
 
-        if(currentNode.getChildren() == null){
+        if (currentNode.getChildren().size() == 0) {
             System.out.println("finished");
-           // this.finalScore = evaluator.evaluate(currentNode.getData());
-        }
+            int value = evaluator.evaluate(currentNode.getData());
+            System.out.println("print evaluation value " + evaluator.evaluate(currentNode.getData()));
+            getMaxBoard();
+            //if(value > maxValue){
+            maxValue = value;
+            if(currentNode.getData() != null) this.bestNode = currentNode;
+            // }
+            return value;
 
-        else {
+        } else {
             if (currentPlayer == 1) { //for max
-                if(this.move != null){
-                    this.move.clear();
-                }
 
                 for (Node<Board> child : currentNode.getChildren()) {
                     this.move.add(child);
-                    this.indexes = new int[currentNode.getChildren().size()];
-                    for (int i = 0; i < currentNode.getChildren().size(); i++) {
-                        this.indexes[i] = evaluator.evaluate(child.getData());//store all the move values for the children
-                    }
+                }
+                System.out.println("childrenSize" + this.move.size());
+
+                this.indexes = new int[currentNode.getChildren().size()];
+                for (int i = 0; i < currentNode.getChildren().size(); i++) {
+                    this.indexes[i] = evaluator.evaluate(move.get(i).getData());
                 }
 
-                this.maximum = indexes[0];
 
+                this.maximum = Integer.MIN_VALUE;
                 for (int i = 0; i < indexes.length; i++) {
                     if (this.indexes[i] > this.maximum)
                         this.maximum = this.indexes[i];
-                        this.bestNode = this.move.get(i);
+                    this.bestNode = this.move.get(i);
+                    // System.out.println("FINAL max score" + this.move.get(i).getData()); //raar
                 }
 
                 currentPlayer = -1 * currentPlayer;
                 System.out.println("Maximal");
-
+                System.out.println("best score max: " + evaluator.evaluate(bestNode.getData()));
+                bestNode.getData().displayBoardGrid();
+                this.move.clear();
+                this.indexes = null;
                 search(bestNode, currentPlayer); //check if the score has to be processed.
             }
 
-            if(currentPlayer == -1){ //for min
-                if(this.move != null){
-                    this.move.clear();
-                }
-
+            if (currentPlayer == -1) { //for min
                 for (Node<Board> child : currentNode.getChildren()) {
                     this.move.add(child);
-                    this.indexes = new int[currentNode.getChildren().size()];
-                    for (int i = 0; i < currentNode.getChildren().size(); i++) {
-                        this.indexes[i] = evaluator.evaluate(child.getData());//store all the move values for the children
-                    }
                 }
 
-                this.minimum = indexes[0];
+                System.out.println("childrenSize" + this.move.size());
 
-                for (int i = 0; i < indexes.length; i++) {
-                    if (this.indexes[i] < this.minimum)
-                        this.minimum = this.indexes[i];
-                        this.bestNode = this.move.get(i);
+                this.indexes = new int[currentNode.getChildren().size()];
+                for (int i = 0; i < currentNode.getChildren().size(); i++) {
+                    this.indexes[i] = evaluator.evaluate(this.move.get(i).getData());//store all the move values for the children
                 }
-
-                currentPlayer = -1 * currentPlayer;
-                System.out.println("Minimal");
-                this.bestNode.getData().displayBoardGrid();
-                search(bestNode, currentPlayer); //check if the score has to be processed.
             }
+            System.out.println("indexSIZE" + indexes.length);
+
+            this.minimum = Integer.MAX_VALUE;
+            for (int i = 0; i < indexes.length; i++) {
+                if (this.indexes[i] < this.minimum)
+                    this.minimum = this.indexes[i];
+                this.bestNode = this.move.get(i);
+            }
+
+            currentPlayer = -1 * currentPlayer;
+            System.out.println("Minimal");
+            this.bestNode.getData().displayBoardGrid();
+            this.move.clear();
+            this.indexes = null;
+            System.out.println("best score min: " + evaluator.evaluate(bestNode.getData()));
+            search(bestNode, currentPlayer); //check if the score has to be processed.
+
+
+
         }
-        return null;
+        return 0;
+    }
+
+    public Node<Board> getMaxBoard(){
+        System.out.println("depth" + this.bestNode.getParent().getDepth());
+        while(this.bestNode.getParent() != null && this.bestNode.getParent().getDepth() > 2){
+            this.bestNode = bestNode.getParent();
+        }
+        return bestNode;
     }
 
     public Node<Board> getBestNode(){return bestNode;}
--- src/AI/Tests/MiniMaxAlphaTest.java
+++ src/AI/Tests/MiniMaxAlphaTest.java
@@ -5,7 +5,7 @@ import Core.Board;
 
 public class MiniMaxAlphaTest {
 
-    final static int DEPTH = 3;
+    final static int DEPTH = 12;
 
     public static void main(String[] args) {
 
@@ -14,8 +14,8 @@ public class MiniMaxAlphaTest {
         MiniMaxAlph minimax = new MiniMaxAlph(evaluator, board);
         GameTree gameTree = new GameTree(DEPTH);
         Node<Board> root = gameTree.createTree();
-        minimax.search(root, 1);
-
+        minimax.search(root, 1);                        //1 is AI
+        minimax.getBestNode().getData().displayBoardGrid();
 
     }
 }
