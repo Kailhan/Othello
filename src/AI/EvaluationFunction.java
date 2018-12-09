a69c28c2ecc6a2df09b3dcd27acddd3a6053400e
MartijnHilders <martijnhilders@live.nl>
2018-12-10 00:17:18 +0100

    added the weights
    AB minimax is hard

--- src/AI/EvaluationFunction.java
+++ src/AI/EvaluationFunction.java
@@ -31,22 +31,22 @@ public class EvaluationFunction extends AI{
 
     public void setWeightPoly() {
         this.weightPoly = new double[WEIGHT_POLY_SIZE];
-        this.weightPoly[0] = 40;  //coinWeightPoly0 = 100;
-        this.weightPoly[1] = 0; //coinWeightPoly1 = 0;
-        this.weightPoly[2] = 0; //coinWeightPoly2 = 0;
-        this.weightPoly[3] = 0; //coinWeightPoly3 = 0;
-        this.weightPoly[4] = 200; //cornerWeightPoly0 = 0;       //set to 0 to disable for a while
-        this.weightPoly[5] = 0; //cornerWeightPoly1 = 0;
-        this.weightPoly[6] = 0; //cornerWeightPoly2 = 0;
-        this.weightPoly[7] = 0; //cornerWeightPoly3 = 0;
-        this.weightPoly[8] = 100; //moveWeightPoly0 = 100;
-        this.weightPoly[9] = 0; //moveWeightPoly1 = 0;
-        this.weightPoly[10] = 0; //moveWeightPoly2 = 0;
-        this.weightPoly[11] = 0; //moveWeightPoly3 = 0;
-        this.weightPoly[12] = 200; //territoryWeightPoly0 = 100;
-        this.weightPoly[13] = 0; //territoryWeightPoly1 = 0;
-        this.weightPoly[14] = 0; //territoryWeightPoly2 = 0;
-        this.weightPoly[15] = 0; //territoryWeightPoly3 = 0;
+        this.weightPoly[0] = -638.2467089;  //coinWeightPoly0 = 100;
+        this.weightPoly[1] = 377.936951; //coinWeightPoly1 = 0;
+        this.weightPoly[2] = -470.8216715; //coinWeightPoly2 = 0;
+        this.weightPoly[3] = -280.0405299; //coinWeightPoly3 = 0;
+        this.weightPoly[4] = 1920.731736; //cornerWeightPoly0 = 0;       //set to 0 to disable for a while
+        this.weightPoly[5] = 947.0323143; //cornerWeightPoly1 = 0;
+        this.weightPoly[6] = 20.09849724; //cornerWeightPoly2 = 0;
+        this.weightPoly[7] = 144.5824839; //cornerWeightPoly3 = 0;
+        this.weightPoly[8] = 850.0535637; //moveWeightPoly0 = 100;
+        this.weightPoly[9] = 130.7802043; //moveWeightPoly1 = 0;
+        this.weightPoly[10] = -1106.398786; //moveWeightPoly2 = 0;
+        this.weightPoly[11] = 44.35520982; //moveWeightPoly3 = 0;
+        this.weightPoly[12] = -21.45856893; //territoryWeightPoly0 = 100;
+        this.weightPoly[13] = 859.9369023; //territoryWeightPoly1 = 0;
+        this.weightPoly[14] = -598.4475435; //territoryWeightPoly2 = 0;
+        this.weightPoly[15] = 637.3359287; //territoryWeightPoly3 = 0;
     }
 
     public void setWeightPoly(double[] weightPoly) {
