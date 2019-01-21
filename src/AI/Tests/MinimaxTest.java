8b63af434774ffa4ba243f3c35895c8f4bd3f440
yvarhulshof <yvarhulshof@hotmail.com>
2019-01-21 21:15:39 +0100

    test w/ side switching

--- src/AI/Tests/MinimaxTest.java
+++ src/AI/Tests/MinimaxTest.java
@@ -3,19 +3,25 @@ package AI.Tests;
 import AI.*;
 import Core.Board;
 
+import javax.swing.*;
+
 public class MinimaxTest {
 
-    final static int DEPTH = 3;
-    final static int GAMES = 100;
+    final static int DEPTH = 7;
+    final static int GAMES = 800;
     final static int SIZE  = 8;
+    static int gamesToBeSimmed = 1000;
+    static int winsFirstMove;
+    static int winsSecondMove;
 
     public static void main(String[] args) {
             //test1();
-            long startTime = System.currentTimeMillis();
-            test2();
-            long endTime = System.currentTimeMillis();
-            long runTime = endTime - startTime;
-            System.out.println("runtime:  " + runTime);
+           // long startTime = System.currentTimeMillis();
+        //for(int i=0; i<11; i++)
+        test2();
+            //long endTime = System.currentTimeMillis();
+            //long runTime = endTime - startTime;
+           // System.out.println("runtime:  " + runTime);
             //test3();
         }
 
@@ -40,17 +46,30 @@ public class MinimaxTest {
         Board board = new Board();
         //Minimax m = new Minimax(DEPTH, board);
         long moveTime = 10;
-        MMAB_IterativeDeepening mmab = new MMAB_IterativeDeepening(40);
+
+        MiniMaxAlph mini = new MiniMaxAlph(DEPTH,board);
+
         MMAB_moveOrdering mmab2 = new MMAB_moveOrdering(10);
-        NS_moveOrdering ns_mo = new NS_moveOrdering(10);
-        MCTS mcts = new MCTS(40, MCTS.STANDARD_EXPLORATION_PARAMETER);
+        NS_moveOrdering ns_mo = new NS_moveOrdering(5);
+        //MCTS_TreeReuse mcts = new MCTS_TreeReuse(moveTime, 1.414);
 
-        GenericTest.test(mmab,mcts , GAMES, SIZE);
-        int p1wins = GenericTest.getPlayer1Wins();
-        int p2wins = GenericTest.getPlayer2Wins();
-        System.out.println("p1_wins: " + p1wins);
-        System.out.println("p2_wins: " + p2wins);
 
+        gamesToBeSimmed = (gamesToBeSimmed < 2) ? 2 : gamesToBeSimmed;
+        gamesToBeSimmed = (gamesToBeSimmed % 2 != 0) ? gamesToBeSimmed + 1: gamesToBeSimmed;
+        GenericTest.test(mmab2,s, gamesToBeSimmed/2, 8);
+        winsFirstMove = GenericTest.getPlayer1Wins();
+        int draws = GenericTest.getDraws();
+        GenericTest.test(s, mmab2, gamesToBeSimmed/2, 8);
+        winsSecondMove = GenericTest.getPlayer2Wins();
+        draws = draws + GenericTest.getDraws();
+
+
+        System.out.println("Wins firstMove: " + winsFirstMove);
+        System.out.println("Wins secondMove: " + winsSecondMove);
+        System.out.println("Total draws: " + draws);
+        System.out.println("Total wins: " + (winsSecondMove+winsFirstMove));
+        System.out.println("---------------------------------------");
+        JOptionPane.showMessageDialog(null, "test done");
     }
 
     private static void test3(){
