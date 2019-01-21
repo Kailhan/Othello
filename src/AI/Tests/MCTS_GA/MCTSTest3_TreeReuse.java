97974cc2cb735078d7a84558dd479147498e57e5
Takowski <takowski@outlook.com>
2019-01-21 03:25:56 +0100

    Update MCTSTest3_TreeReuse.java

--- src/AI/Tests/MCTS_GA/MCTSTest3_TreeReuse.java
+++ src/AI/Tests/MCTS_GA/MCTSTest3_TreeReuse.java
@@ -13,23 +13,23 @@ import java.sql.Timestamp;
 public class MCTSTest3_TreeReuse {
 
     public static void main(String[] args) {
-        int minPlayouts = 1;
+        int minPlayouts = 5;
         int maxPlayouts = 10;
         int stepsPlayouts = 1;
         double minExplore = 0;
-        double maxExplore = 5;
-        double stepsExplore = 1;
+        double maxExplore = 2;
+        double stepsExplore = 0.1;
         int scaling = 100;
         int minExploreInt = (int)Math.round(minExplore * scaling);
         int maxExploreInt = (int)Math.round(maxExplore * scaling);
         int stepsExploreInt = (int)Math.round(stepsExplore * scaling);
-        int totalGames = 100;
+        int totalGames = 4;
         int rowWidth = 6;
 
         int logCounter = 0;
         System.out.println((((maxPlayouts - minPlayouts)/stepsPlayouts) + 1));
         System.out.println((((maxExploreInt - minExploreInt)/stepsExploreInt) + 1));
-        String [] log = new String[(((maxPlayouts - minPlayouts)/stepsPlayouts) + 1) * (((maxExploreInt - minExploreInt)/stepsExploreInt) + 1) * rowWidth];
+        String [] log = new String[((((maxPlayouts - minPlayouts)/stepsPlayouts) + 1) * (((maxExploreInt - minExploreInt)/stepsExploreInt) + 1) * rowWidth) + (((maxPlayouts - minPlayouts)/stepsPlayouts) + 1) * (((maxExploreInt - minExploreInt)/stepsExploreInt)+1)];
         //String[] log = new String[660];
         System.out.println("loglength: " +  log.length);
         System.out.println("minExploreInt: " +  minExploreInt);
@@ -37,41 +37,40 @@ public class MCTSTest3_TreeReuse {
         System.out.println("stepsExploreInt: " + stepsExploreInt);
 
         StringBuilder logBuilder8 = new StringBuilder();
-
+        int sometmpcntr =0;
         for(int i = minExploreInt; i <= maxExploreInt; i += stepsExploreInt) {
             long startTimeTot = System.nanoTime();
             for(int j = minPlayouts; j < maxPlayouts + 1; j += stepsPlayouts) {
+                sometmpcntr++;
                 long startTime = System.nanoTime();
                 MCTS_TreeReuse mcts = new MCTS_TreeReuse(j, (double)i/scaling);
-
+                log[logCounter] = String.valueOf(j); logCounter++;
                 log[logCounter] = String.valueOf((double)i/scaling); logCounter++;
-                GenericTest.test(mcts, new Stupid(), totalGames/2, 8);
+                GenericTest.test(mcts, new Stupid(), totalGames/2, 4);
                 log[logCounter] = String.valueOf(GenericTest.getPlayer1Wins()); logCounter++;
                 log[logCounter] = String.valueOf(GenericTest.getDraws()); logCounter++;
-                GenericTest.test(new Stupid(), mcts, totalGames/2, 8);
+                GenericTest.test(new Stupid(), mcts, totalGames/2, 4);
                 log[logCounter] = String.valueOf(GenericTest.getPlayer2Wins()); logCounter++;
                 log[logCounter] = String.valueOf(GenericTest.getDraws()); logCounter++;
                 long endTime = System.nanoTime();
                 log[logCounter] = String.valueOf(endTime-startTime); logCounter++;
-                //System.out.println("board8 iter: " + j + "time: " + ((endTime-startTime)/1000000000));
+                System.out.println("board8 iter: " + j + "time: " + ((endTime-startTime)/1000000000));
             }
             long endTimeTot = System.nanoTime();
             System.out.println("logCntr:" + logCounter);
             System.out.println("Done with exploreparam: " + (double)i/scaling);
             System.out.println(((endTimeTot-startTimeTot)/1000000000));
         }
+        System.out.println("tmpcntr" +sometmpcntr);
         System.out.println("Done with board 8: " + System.currentTimeMillis());
 
         logBuilder8.append("maxPlayouts").append(",").append("explorationParam").append(",").append("board(8)_p1").append(",").append("draws").append(",").append("board(8)_p2").append(",").append("draws").append(",").append("iterTime");
         for(int i = 0; i < log.length; i++) {
-            if(i % rowWidth == 0) {
+            if(i % (rowWidth+1) == 0) {
                 logBuilder8.append("\n");
-                System.out.println((minPlayouts + ((i/rowWidth)*stepsPlayouts)));
-                int playout = ((minPlayouts + ((i/rowWidth)*stepsPlayouts))%maxPlayouts == 0) ? maxPlayouts : (minPlayouts + ((i/rowWidth)*stepsPlayouts)) % maxPlayouts;
-                logBuilder8.append(playout).append(",");
             }
             logBuilder8.append(log[i]);
-            if((i+1) % rowWidth != 0) logBuilder8.append(",");
+            if((i+1) % (rowWidth+1) != 0) logBuilder8.append(",");
         }
 
         try {
